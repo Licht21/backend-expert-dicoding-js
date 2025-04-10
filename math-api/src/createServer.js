6fa50afd040c3e4fa4bd1177679ad1edde9e6abc
yusuf <yusuf210409@gmail.com>
2025-04-11 00:15:27 +0700

    finishing function test

--- math-api/src/createServer.js
+++ math-api/src/createServer.js
@@ -1,6 +1,6 @@
 const Hapi = require('@hapi/hapi')
 
-const createServer = ({mathBasic}) => {
+const createServer = ({mathBasic, figureCalculator}) => {
     const server = Hapi.server({
         host: 'localhost',
         port: 5000
@@ -31,6 +31,51 @@ const createServer = ({mathBasic}) => {
             handler: (request) => {
                 const {a, b} = request.params
                 const value = mathBasic.multiply(Number(a), Number(b))
+                return {value}
+            }
+        },
+        {
+            method: 'GET',
+            path: '/divide/{a}/{b}',
+            handler: (request) => {
+                const {a, b} = request.params
+                const value = mathBasic.divide(Number(a), Number(b))
+                return {value}
+            }
+        },
+        {
+            method: 'GET',
+            path: '/rectangle/perimeter/{length}/{width}',
+            handler: (request) => {
+                const {length, width} = request.params
+                const value = figureCalculator.calculateRectanglePerimeter(Number(length),Number(width))
+                return {value}
+            }
+        },
+        {
+            method: 'GET',
+            path: '/rectangle/area/{length}/{width}',
+            handler: (request) => {
+                const {length, width} = request.params
+                const value = figureCalculator.calculateRectangleArea(Number(length), Number(width))
+                return {value}
+            }
+        },
+        {
+            method: 'GET',
+            path: '/triangle/perimeter/{sideA}/{sideB}/{base}',
+            handler: (request) => {
+                const {sideA, sideB, base} = request.params
+                const value = figureCalculator.calculateTrianglePerimeter(Number(sideA), Number(sideB), Number(base))
+                return {value}
+            }
+        },
+        {
+            method: 'GET',
+            path: '/triangle/area/{height}/{base}',
+            handler: (request) => {
+                const {height, base} = request.params
+                const value = figureCalculator.calculateTriangleArea(Number(height),Number(base))
                 return {value}
             }
         }
--- math-api/src/createServer.test.js
+++ math-api/src/createServer.test.js
@@ -1,4 +1,5 @@
 const createServer = require('./createServer')
+const FigureCalculator = require('./FigureCalculator')
 const MathBasic = require('./MathBasic')
 
 describe('A HTTP Server', () => {
@@ -65,6 +66,126 @@ describe('A HTTP Server', () => {
             expect(response.statusCode).toEqual(200)
             expect(responseJson.value).toEqual(100)
             expect(spyMultiply).toHaveBeenCalledWith(a, b)
+        })
+    })
+
+    describe('when GET divide', () => {
+        it('should respond with a status of 200 and the payload values dividing result', async () => {
+            // Arrange
+            const a = 10
+            const b = 2
+            const spyDivide = jest.spyOn(MathBasic, 'divide')
+            const server = createServer({mathBasic: MathBasic})
+
+            // Actions
+            const response = await server.inject({
+                method: 'GET',
+                url: `/divide/${a}/${b}`
+            })
+
+            // Assert
+            const responseJson = JSON.parse(response.payload)
+            expect(response.statusCode).toEqual(200)
+            expect(responseJson.value).toEqual(5)
+            expect(spyDivide).toHaveBeenCalledWith(a,b)
+        })
+    })
+
+    describe('when GET rectangle perimeter', () => {
+        it('should response with a status of 200 and the payload values rectangle perimeter', async () => {
+            // Arrange
+            const length = 4
+            const width = 2
+            const spyAdd = jest.spyOn(MathBasic, 'add')
+            const spyMultiply = jest.spyOn(MathBasic, 'multiply')
+            const figureCalculator = new FigureCalculator(MathBasic)
+            const server = createServer({figureCalculator})
+
+            // Actions
+            const response = await server.inject({
+                method: 'GET',
+                url: `/rectangle/perimeter/${length}/${width}`
+            })
+
+            // Assert
+            const responseJson = JSON.parse(response.payload)
+            expect(response.statusCode).toEqual(200)
+            expect(responseJson.value).toEqual(12)
+            expect(spyAdd).toHaveBeenCalledWith(length, width)
+            expect(spyMultiply).toHaveBeenCalledWith(2,length + width)
+        })
+    })
+
+    describe('when GET rectangle area', () => {
+        it('should respond with a status of 200 and the payload values rectangle area', async () => {
+            // Arrange
+            const length = 10
+            const width = 8
+            const spyMultiply = jest.spyOn(MathBasic, 'multiply')
+            const figureCalculator = new FigureCalculator(MathBasic)
+            const server = createServer({figureCalculator})
+
+            // Actions
+            const response = await server.inject({
+                method: 'GET',
+                url: `/rectangle/area/${length}/${width}`
+            })
+
+            // Assert
+            const responseJson = JSON.parse(response.payload)
+            expect(response.statusCode).toEqual(200)
+            expect(responseJson.value).toEqual(80)
+            expect(spyMultiply).toHaveBeenCalledWith(length, width)
+        })
+    })
+
+    describe('when GET triangle perimeter', () => {
+        it('should respond with a status 200 and the payload values trianle perimeter', async () => {
+            // Arrange
+            const sideA = 10
+            const sideB = 10
+            const base = 10
+            const spyAdd = jest.spyOn(MathBasic, 'add')
+            const figureCalculator = new FigureCalculator(MathBasic)
+            const server = createServer({figureCalculator})
+
+            // Actions
+            const response = await server.inject({
+                method: 'GET',
+                url: `/triangle/perimeter/${sideA}/${sideB}/${base}`
+            })
+
+            // Assert
+            const responseJson = JSON.parse(response.payload)
+            expect(response.statusCode).toEqual(200)
+            expect(responseJson.value).toEqual(sideA + sideB + base)
+            expect(spyAdd).toHaveBeenCalledWith(10,10)
+            expect(spyAdd).toHaveBeenCalledWith(20,10)
+        })
+    })
+
+    describe('when GET triangle area', () => {
+        it('should respond with a status 200 and the payload values triangle area', async () => {
+            // Arrange
+            const height = 10
+            const base = 10
+            const spyMultiply = jest.spyOn(MathBasic, 'multiply')
+            const spyDivide = jest.spyOn(MathBasic, 'divide')
+            const figureCalculator = new FigureCalculator(MathBasic)
+            const server = createServer({figureCalculator})
+
+            // Actions
+            const response = await server.inject({
+                method:'GET',
+                url: `/triangle/area/${height}/${base}`
+            })
+
+            // Assert
+            const responseJson = JSON.parse(response.payload)
+            expect(response.statusCode).toEqual(200)
+            expect(responseJson.value).toEqual(50)
+            expect(spyMultiply).toHaveBeenCalledWith(height, base)
+            expect(spyDivide).toHaveBeenCalledWith(height * base,2)
         })
     })
 })
